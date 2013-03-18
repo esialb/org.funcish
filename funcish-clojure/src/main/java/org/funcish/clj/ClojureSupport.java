36e819697b4fa860b828f57443525e5e5ce8c503
Robin Kirkman <robin@robinkirkman.com>
2013-03-18 09:21:41 -0400

    Some more ClojureSupport functions and a test

--- funcish-clojure/src/main/java/org/funcish/clj/ClojureSupport.java
+++ funcish-clojure/src/main/java/org/funcish/clj/ClojureSupport.java
@@ -1,19 +1,38 @@
 package org.funcish.clj;
 
+import org.funcish.core.Reducers;
 import org.funcish.core.fn.Function;
+import org.funcish.core.fn.Reducator;
+import org.funcish.core.fn.Reducer;
 
 import clojure.lang.IFn;
 import clojure.lang.RestFn;
 
 public class ClojureSupport {
 	
-	public static <T> IFn toClojure(Function<T> fn) {
+	public static <T> IFn clojureIFn(Function<T> fn) {
 		return new ProxyFunctionIFn<T>(fn);
 	}
 	
-	public static Function<Object> fromClojure(RestFn fn) {
+	public static Function<Object> function(RestFn fn) {
 		return new RestFnProxyFunction(fn);
 	}
 	
+	public static Reducer<Object, Object> reducer(RestFn fn) {
+		return Reducers.reducer(Object.class, function(fn));
+	}
+	
+	public static Reducer<Object, Object> reducer(RestFn fn, Object memoStart) {
+		return Reducers.reducer(Object.class, memoStart, function(fn));
+	}
+	
+	public static Reducator<Object, Object> reducator(RestFn fn) {
+		return Reducers.reducator(reducer(fn));
+	}
+
+	public static Reducator<Object, Object> reducator(RestFn fn, Object memoStart) {
+		return Reducers.reducator(reducer(fn, memoStart));
+	}
+
 	private ClojureSupport() {}
 }
--- funcish-clojure/src/test/clojure/org/funcish/clj/ClojureSupportTest.clj
+++ funcish-clojure/src/test/clojure/org/funcish/clj/ClojureSupportTest.clj
@@ -3,11 +3,14 @@
 (use 'clojure.test)
 (use 'clojure.test.junit)
 
+(import '(org.funcish.core Functions Mappings Predicates Reducers))
 (import '(org.funcish.clj ClojureSupport))
+
 
 (with-junit-output
   (run-tests 'org.funcish.clj.ClojureSupportTest))
 
 (deftest clojure-support-fns
-  (is (ClojureSupport/fromClojure +))
+  (is (ClojureSupport/function +))
+  (is (= 6 (.reduce (ClojureSupport/reducator + 0) '(1 2 3))))
   )
