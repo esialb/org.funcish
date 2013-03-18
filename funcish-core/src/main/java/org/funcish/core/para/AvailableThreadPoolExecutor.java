db0f8c3507982b5b8c5bcc358fa15a61055d8c65
Robin Kirkman <robin@robinkirkman.com>
2013-03-18 03:27:49 -0400

    ThreadPoolExecutor that grows and shrinks with the available # of CPUs.

---
+++ funcish-core/src/main/java/org/funcish/core/para/AvailableThreadPoolExecutor.java
@@ -0,0 +1,41 @@
+package org.funcish.core.para;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class AvailableThreadPoolExecutor extends ThreadPoolExecutor {
+
+	private int threadsPerProcessor;
+	
+	public AvailableThreadPoolExecutor(int threadsPerProcessor) {
+		this(threadsPerProcessor, 30, TimeUnit.SECONDS);
+	}
+	
+	public AvailableThreadPoolExecutor(int threadsPerProcessor, long keepAliveTime, TimeUnit unit) {
+		super(1, Integer.MAX_VALUE, keepAliveTime, unit, new LinkedBlockingQueue<Runnable>());
+		this.threadsPerProcessor = threadsPerProcessor;
+	}
+	
+	protected void adjustPool() {
+		int idealThreads = threadsPerProcessor * Runtime.getRuntime().availableProcessors();
+		if(idealThreads > getCorePoolSize())
+			setCorePoolSize(getCorePoolSize() + 1);
+		else if(idealThreads < getCorePoolSize() && getActiveCount() < idealThreads- 1)
+			setCorePoolSize(Math.max(getCorePoolSize() - 1, 1));
+	}
+	
+	@Override
+	protected void beforeExecute(Thread t, Runnable r) {
+		super.beforeExecute(t, r);
+		adjustPool();
+	}
+	
+	@Override
+	protected void afterExecute(Runnable r, Throwable t) {
+		super.afterExecute(r, t);
+		adjustPool();
+	}
+
+}
