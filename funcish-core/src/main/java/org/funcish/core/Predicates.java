48641ecc4d526771b3a6b024683678dfb5cd0bb9
Robin Kirkman <robin@robinkirkman.com>
2013-03-19 13:45:22 -0400

    Some boolean predicators

--- funcish-core/src/main/java/org/funcish/core/Predicates.java
+++ funcish-core/src/main/java/org/funcish/core/Predicates.java
@@ -5,6 +5,7 @@ import org.funcish.core.fn.ParaPredicator;
 import org.funcish.core.fn.Predicate;
 import org.funcish.core.fn.Predicator;
 import org.funcish.core.impl.AbstractPredicate;
+import org.funcish.core.impl.AbstractPredicator;
 import org.funcish.core.impl.ProxyParaPredicator;
 import org.funcish.core.impl.ProxyPredicate;
 import org.funcish.core.impl.ProxyPredicator;
@@ -23,14 +24,85 @@ public class Predicates {
 		return new ProxyParaPredicator<T>(target);
 	}
 	
-	public static <T, U extends T> Predicate<U> narrow(Class<U> u, final Predicate<T> test) {
-		return new NarrowingPredicate<T, U>(u, test);
+	public static <T, U extends T> Predicator<U> narrow(Class<U> u, Predicate<T> test) {
+		return new NarrowingPredicator<T, U>(u, test);
 	}
 	
-	private static class NarrowingPredicate<T, U extends T> extends AbstractPredicate<U> {
-		private final Predicate<T> test;
+	public static <T> Predicator<T> and(Predicate<T> first, Predicate<T>... rest) {
+		return new AndPredicator<T>(first.t(), rest, first);
+	}
 	
-		private NarrowingPredicate(Class<U> t, Predicate<T> test) {
+	public static <T> Predicator<T> or(Predicate<T> first, Predicate<T>... rest) {
+		return new OrPredicator<T>(first.t(), first, rest);
+	}
+	
+	public static <T> Predicator<T> not(Predicate<T> p) {
+		return new NotPredicator<T>(p.t(), p);
+	}
+	
+	private static class NotPredicator<T> extends AbstractPredicator<T> {
+		private Predicate<T> p;
+
+		private NotPredicator(Class<T> t, Predicate<T> p) {
+			super(t);
+			this.p = p;
+		}
+
+		@Override
+		public boolean test0(T value, Integer index) throws Exception {
+			return !p.test(value, index);
+		}
+	}
+
+	private static class AndPredicator<T> extends AbstractPredicator<T> {
+		private Predicate<T>[] rest;
+		private Predicate<T> first;
+
+		private AndPredicator(Class<T> t, Predicate<T>[] rest,
+				Predicate<T> first) {
+			super(t);
+			this.rest = rest;
+			this.first = first;
+		}
+
+		@Override
+		public boolean test0(T value, Integer index) throws Exception {
+			if(!first.test(value, index))
+				return false;
+			for(Predicate<T> p : rest) {
+				if(!p.test(value, index))
+					return false;
+			}
+			return true;
+		}
+	}
+
+	private static class OrPredicator<T> extends AbstractPredicator<T> {
+		private Predicate<T> first;
+		private Predicate<T>[] rest;
+
+		private OrPredicator(Class<T> t, Predicate<T> first, Predicate<T>[] rest) {
+			super(t);
+			this.first = first;
+			this.rest = rest;
+		}
+
+		@Override
+		public boolean test0(T value, Integer index) throws Exception {
+			if(first.test(value, index))
+				return true;
+			for(Predicate<T> p : rest) {
+				if(p.test(value, index))
+					return true;
+			}
+			return false;
+		}
+	}
+
+	private static class NarrowingPredicator<T, U extends T> extends AbstractPredicator<U> {
+		private Predicate<T> test;
+	
+		private NarrowingPredicator(Class<U> t, Predicate<T> test) {
 			super(t);
 			this.test = test;
 		}
