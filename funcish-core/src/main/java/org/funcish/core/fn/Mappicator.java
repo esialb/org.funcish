b184b16ba39ef81bae5e44b70b939478f4af61e5
Robin Kirkman <robin@robinkirkman.com>
2013-03-20 03:27:13 -0400

    More javadoc

--- funcish-core/src/main/java/org/funcish/core/fn/Mappicator.java
+++ funcish-core/src/main/java/org/funcish/core/fn/Mappicator.java
@@ -10,6 +10,19 @@ import java.util.Collection;
  * @param <V>
  */
 public interface Mappicator<K, V> extends Mapping<K, V>, Applicator<K, Collection<V>, V> {
+	/**
+	 * Return a new {@link Collection} containing the output values obtained by applying
+	 * this {@link Mappicator} to the input
+	 * @param c
+	 * @return
+	 */
 	public Collection<V> map(Collection<? extends K> c);
+	/**
+	 * Apply this {@link Mappicator} to the input {@link Collection} {@code c}, and add
+	 * the results to the output {@link Collection} {@code into}
+	 * @param c
+	 * @param into
+	 * @return
+	 */
 	public <C extends Collection<? super V>> C map(Collection<? extends K> c, C into);
 }
--- funcish-core/src/main/java/org/funcish/core/fn/ParaMappicator.java
+++ funcish-core/src/main/java/org/funcish/core/fn/ParaMappicator.java
@@ -3,7 +3,31 @@ package org.funcish.core.fn;
 import java.util.Collection;
 import java.util.concurrent.Executor;
 
+/**
+ * Provides parallelized {@link Mappicator} functionality
+ * @author robin
+ *
+ * @param <K>
+ * @param <V>
+ */
 public interface ParaMappicator<K, V> extends Mappicator<K, V>, ParaApplicator<K, Collection<V>, V> {
+	/**
+	 * Apply this {@link ParaMappicator} to the input {@link Collection} {@code c}, with
+	 * each application in a separate job submitted to the {@link Executor} {@code exec},
+	 * returning the results as a new {@link Collection}
+	 * @param exec
+	 * @param c
+	 * @return
+	 */
 	public Collection<V> map(Executor exec, Collection<? extends K> c);
+	/**
+	 * Apply this {@link ParaMappicator} to the input {@link Collection} {@code c} with each
+	 * application in a separate job submitted to the {@link Executor} {@code exec},
+	 * adding the results to the output {@link Collection} {@code into}
+	 * @param exec
+	 * @param c
+	 * @param into
+	 * @return
+	 */
 	public <C extends Collection<? super V>> C map(Executor exec, Collection<? extends K> c, C into);
 }
