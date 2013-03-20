5c8fa94bcb667ae2930ba8fae66cc3ab1c3cbe2b
Robin Kirkman <robin@robinkirkman.com>
2013-03-20 03:33:51 -0400

    More javadoc

--- funcish-core/src/main/java/org/funcish/core/fn/Reducator.java
+++ funcish-core/src/main/java/org/funcish/core/fn/Reducator.java
@@ -2,7 +2,25 @@ package org.funcish.core.fn;
 
 import java.util.Collection;
 
+/**
+ * {@link Applicator} which can applies a {@link Reducer} to a {@link Collection}
+ * @author robin
+ *
+ * @param <E>
+ * @param <M>
+ */
 public interface Reducator<E, M> extends Reducer<E, M>, Applicator<E, M, M> {
+	/**
+	 * Reduce the argument {@link Collection} using the default initial value
+	 * @param c
+	 * @return
+	 */
 	public M reduce(Collection<? extends E> c);
+	/**
+	 * Reduce the argumetn {@link Collection} using the argument initial value
+	 * @param c
+	 * @param into
+	 * @return
+	 */
 	public M reduce(Collection<? extends E> c, M into);
 }
--- funcish-core/src/main/java/org/funcish/core/fn/Reducer.java
+++ funcish-core/src/main/java/org/funcish/core/fn/Reducer.java
@@ -1,8 +1,36 @@
 package org.funcish.core.fn;
 
+import java.util.Collection;
+
+/**
+ * {@link Function} to reduce an input collection to an output value
+ * @author robin
+ *
+ * @param <E>
+ * @param <M>
+ */
 public interface Reducer<E, M> extends Function<M> {
+	/**
+	 * The class of the input collection's elements
+	 * @return
+	 */
 	public Class<E> e();
+	/**
+	 * The class of the output value
+	 * @return
+	 */
 	public Class<M> m();
+	/**
+	 * The initial output value
+	 * @return
+	 */
 	public M memoStart();
+	/**
+	 * Perform a step in the reduction of a {@link Collection}.
+	 * @param memo The previous step's output value
+	 * @param obj The object to consider
+	 * @param index The object's index
+	 * @return The new output value
+	 */
 	public M reduce(M memo, E obj, Integer index);
 }
