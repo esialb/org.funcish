ab4e71d3aca588f344e1e5cd6311d90afc917a2b
Robin Kirkman <robin@robinkirkman.com>
2013-03-20 23:29:42 -0400

    min/max reducators

--- funcish-core/src/main/java/org/funcish/core/Reducers.java
+++ funcish-core/src/main/java/org/funcish/core/Reducers.java
@@ -1,5 +1,8 @@
 package org.funcish.core;
 
+import java.util.Collection;
+import java.util.Comparator;
+
 import org.funcish.core.fn.Function;
 import org.funcish.core.fn.ParaReducator;
 import org.funcish.core.fn.Reducator;
@@ -71,6 +74,110 @@ public class Reducers {
 		return new NarrowingReducator<E, F, M>(f, reducer.m(), reducer.memoStart(), reducer);
 	}
 	
+	/**
+	 * Return a new {@link Reducator} that locates the minimum element of a {@link Collection}
+	 * @param e
+	 * @param cmp
+	 * @return
+	 */
+	public static <E> Reducator<E, E> min(Class<E> e, Comparator<? super E> cmp) {
+		return new ComparatorMin<E>(e, e, cmp);
+	}
+	
+	/**
+	 * Return a new {@link Reducator} that locates the minimum element of a {@link Collection}
+	 * @param e
+	 * @return
+	 */
+	public static <E extends Comparable<? super E>> Reducator<E, E> min(Class<E> e) {
+		return new ComparableMin<E>(e, e);
+	}
+
+	/**
+	 * Returns a new {@link Reducator} that locates the maximum element of a {@link Collection}
+	 * @param e
+	 * @param cmp
+	 * @return
+	 */
+	public static <E> Reducator<E, E> max(Class<E> e, Comparator<? super E> cmp) {
+		return new ComparatorMax<E>(e, e, cmp);
+	}
+	
+	/**
+	 * Returns a new {@link Reducator} that locates the maximum element of a {@link Collection}
+	 * @param e
+	 * @return
+	 */
+	public static <E extends Comparable<? super E>> Reducator<E, E> max(Class<E> e) {
+		return new ComparableMax<E>(e, e);
+	}
+	
+	private static class ComparableMax<E extends Comparable<? super E>> extends AbstractReducator<E, E> {
+		private ComparableMax(Class<E> e, Class<E> m) {
+			super(e, m, null);
+		}
+
+		@Override
+		public E reduce0(E memo, E obj, Integer index) throws Exception {
+			if(memo == null)
+				return obj;
+			if(memo.compareTo(obj) < 0)
+				return obj;
+			return memo;
+		}
+	}
+
+	private static class ComparableMin<E extends Comparable<? super E>> extends AbstractReducator<E, E> {
+		private ComparableMin(Class<E> e, Class<E> m) {
+			super(e, m, null);
+		}
+
+		@Override
+		public E reduce0(E memo, E obj, Integer index) throws Exception {
+			if(memo == null)
+				return obj;
+			if(memo.compareTo(obj) > 0)
+				return obj;
+			return memo;
+		}
+	}
+
+	private static class ComparatorMax<E> extends AbstractReducator<E, E> {
+		private final Comparator<? super E> cmp;
+
+		private ComparatorMax(Class<E> e, Class<E> m, Comparator<? super E> cmp) {
+			super(e, m, null);
+			this.cmp = cmp;
+		}
+
+		@Override
+		public E reduce0(E memo, E obj, Integer index) throws Exception {
+			if(memo == null)
+				return obj;
+			if(cmp.compare(memo, obj) < 0)
+				return obj;
+			return memo;
+		}
+	}
+
+	private static class ComparatorMin<E> extends AbstractReducator<E, E> {
+		private final Comparator<? super E> cmp;
+
+		private ComparatorMin(Class<E> e, Class<E> m, Comparator<? super E> cmp) {
+			super(e, m, null);
+			this.cmp = cmp;
+		}
+
+		@Override
+		public E reduce0(E memo, E obj, Integer index) throws Exception {
+			if(memo == null)
+				return obj;
+			if(cmp.compare(memo, obj) > 0)
+				return obj;
+			return memo;
+		}
+	}
+
 	private static class NarrowingReducator<E, F extends E, M> extends AbstractReducator<F, M> {
 		private final Reducer<E, M> reducer;
 	
