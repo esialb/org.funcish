5ca5c4177a82e8e69410fe36d1693fa66ce27ac8
Robin Kirkman <robin@robinkirkman.com>
2013-03-26 04:25:40 -0400

    Tweak Sequences API

--- funcish-core/src/main/java/org/funcish/core/Sequences.java
+++ funcish-core/src/main/java/org/funcish/core/Sequences.java
@@ -10,33 +10,44 @@ import org.funcish.core.impl.ProxySequence;
 
 public class Sequences {
 	
-	public static <E> Sequence<E> sequence(Class<E> e, Function<E> target) {
+	public static <E> Sequence<E> sequence(Class<E> e, Function<? extends E> target) {
 		return new ProxySequence<E>(e, target);
 	}
 	
-	public static <E> Sequencator<E> sequencator(Class<E> e, Sequence<E> target) {
-		if(target instanceof Sequencator<?>)
-			return (Sequencator<E>) target;
+	public static <E> Sequencator<E> sequencator(Class<E> e, Sequence<? extends E> target) {
+		if(target instanceof Sequencator<?>) {
+			final Sequencator<? extends E> s = (Sequencator<? extends E>) target;
+			return new WideningSequencator<E>(e, s);
+		}
 		return new ProxySequencator<E>(e, target);
 	}
 	
 	public static <E> Sequencator<E> widen(Class<E> e, final Sequencator<? extends E> sequence) {
-		return new AbstractSequencator<E>(e) {
-			@Override
-			public boolean hasNext0(Integer index) throws Exception {
-				return sequence.hasNext(index);
-			}
-
-			@Override
-			public E next0(Integer index) throws Exception {
-				return sequence.next(index);
-			}
-		};
+		return sequencator(e, sequence);
 	}
 	
 	public static <E> Sequence<E> widen(Class<E> e, final Sequence<? extends E> sequence) {
-		return new ProxySequence<E>(e, sequence);
+		return sequence(e, sequence);
 	}
 	
+	private static class WideningSequencator<E> extends AbstractSequencator<E> {
+		private final Sequencator<? extends E> s;
+	
+		private WideningSequencator(Class<E> e, Sequencator<? extends E> s) {
+			super(e);
+			this.s = s;
+		}
+	
+		@Override
+		public boolean hasNext0(Integer index) throws Exception {
+			return s.hasNext(index);
+		}
+	
+		@Override
+		public E next0(Integer index) throws Exception {
+			return s.next(index);
+		}
+	}
+
 	private Sequences() {}
 }
