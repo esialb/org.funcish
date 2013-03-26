acee691e8bba20ccc092336d06e06c7553a45fed
Robin Kirkman <robin@robinkirkman.com>
2013-03-26 05:24:36 -0400

    Declare a thrown exception for Sequence

--- funcish-core/src/main/java/org/funcish/core/fn/Sequence.java
+++ funcish-core/src/main/java/org/funcish/core/fn/Sequence.java
@@ -1,9 +1,10 @@
 package org.funcish.core.fn;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public interface Sequence<E> extends Function<E> {
 	public Class<E> e();
 	
-	public E next(Integer index);
+	public E next(Integer index) throws NoSuchElementException;
 }
