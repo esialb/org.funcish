a2771f774fe3acbf908e7242e35e209f7e7c14a1
Robin Kirkman <robin@robinkirkman.com>
2013-07-19 22:10:09 -0400

    Refactor widening multimapper to an inner class

--- funcish-core/src/main/java/org/funcish/core/util/MultiMappings.java
+++ funcish-core/src/main/java/org/funcish/core/util/MultiMappings.java
@@ -16,16 +16,25 @@ public class MultiMappings {
 	}
 	
 	public static <K, U, V extends U> MultiMapper<K, U> widen(Class<U> u, final MultiMapping<K, V> m) {
-		return new AbstractMultiMapper<K, U>(m.k(), u) {
-
-			@Override
-			public void map0(K key, MultiReceiver<? super U> receiver, Integer index)
-					throws Exception {
-				m.map(key, receiver, index);
-			}
-		};
+		return new WideningMultiMapper<K, U, V>(m.k(), u, m);
 	}
 	
+	private static class WideningMultiMapper<K, U, V extends U> extends
+			AbstractMultiMapper<K, U> {
+		private final MultiMapping<K, V> m;
+
+		private WideningMultiMapper(Class<K> k, Class<U> v, MultiMapping<K, V> m) {
+			super(k, v);
+			this.m = m;
+		}
+
+		@Override
+		public void map0(K key, MultiReceiver<? super U> receiver, Integer index)
+				throws Exception {
+			m.map(key, receiver, index);
+		}
+	}
+
 	private static class NarrowingMultiMapper<K, L extends K, V> 
 	extends AbstractMultiMapper<L, V> {
 		private final MultiMapping<K, V> m;
